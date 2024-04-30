c2caeb49a00b739508428e84f88b6f05f911a126
DaryaBorovinskaya <dashh17@gmail.com>
2024-04-30 23:36:39 +0700

    Пока так, а там посмотрим

---
+++ ModelingLab2/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingLab2
+{
+    public class Application
+    {
+        private int _number;
+        private double _timeComing;
+        private double _timeWaitInBuff;
+
+        public int Number { get => _number; set => _number = value; }
+        public double TimeComing { get => _timeComing; set => _timeComing = value; }
+        public double TimeWaitInBuff { get => _timeWaitInBuff; set => _timeWaitInBuff = value; }
+
+        public Application(int number, double timeComing, double timeWaitInBuff)
+        {
+            Number = number;
+            TimeComing = timeComing;
+            TimeWaitInBuff = timeWaitInBuff;
+        }
+    }
+}
---
+++ ModelingLab2/PerformanceIndicators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingLab2
+{
+    /// <summary>
+    /// Показатели эффективности
+    /// </summary>
+    public class PerformanceIndicators
+    {
+        public double CalculateCoeffWorkload( double Tstagn1,   double Tstagn2,  double Tmod)
+        {
+            return Tstagn1 / (Tmod - Tstagn1) * Tstagn2 / (Tmod - Tstagn2);
+        }
+        public double CalculateT_averServ( double Tserv,  double Twait1,  double Twait2,  double Nserv)
+        {
+            return (Tserv + Twait1 + Twait2) / Nserv;
+        }
+        public double CalculateP_noServ( double N_noServ,  double N)
+        {
+            return N_noServ / N;
+        }
+    }
+}
