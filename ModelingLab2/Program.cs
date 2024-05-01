9020e8cc86b6b884ccc9f48be8abc3fd138b3e01
DaryaBorovinskaya <dashh17@gmail.com>
2024-05-01 20:15:30 +0700

    1

--- ModelingLab2/Program.cs
+++ ModelingLab2/Program.cs
@@ -8,18 +8,23 @@ Console.ForegroundColor = ConsoleColor.DarkMagenta;
 ModelingProcess modelingProcess = new ();
 StatisticalStability statistical = new();
 (decimal coeffWorkload, decimal T_averServ, decimal P_noServ) result = modelingProcess.StartProcess();
-int countOfRealization = 50;
+//int countOfRealization = 50;
 
-decimal[] p = statistical.FindProbability(out countOfRealization);
-for (int i = 0; i < p.Length; i++)
-{
-    Console.WriteLine(p[i]);
-}
+//decimal[] p = statistical.FindProbability(out countOfRealization);
+//for (int i = 0; i < p.Length; i++)
+//{
+//    Console.WriteLine(p[i]);
+//}
 
-Console.WriteLine($"\nВремя моделирования: 1000");
-Console.WriteLine($"Погрешность: 0,04");
-Console.WriteLine($"Необходимое число реализаций: {countOfRealization}");
-//Console.WriteLine("\nРасчёт показателей эффективности функционирования системы:");
-//Console.WriteLine($"\nКоэффициент загруженности: {result.coeffWorkload}");
-//Console.WriteLine($"Среднее время обслуживания: {result.T_averServ}");
-//Console.WriteLine($"Вероятность попадания в отложенные: {result.P_noServ}");
+//Console.WriteLine($"\nВремя моделирования: 1000");
+//Console.WriteLine($"Погрешность: 0,04");
+//Console.WriteLine($"Необходимое число реализаций: {countOfRealization}");
+
+
+
+
+
+Console.WriteLine("\nРасчёт показателей эффективности функционирования системы:");
+Console.WriteLine($"\nКоэффициент загруженности: {result.coeffWorkload}");
+Console.WriteLine($"Среднее время обслуживания: {result.T_averServ}");
+Console.WriteLine($"Вероятность попадания в отложенные: {result.P_noServ}");
